058cdae4d84626fe9cfd389e368ad3c3c9122019
lesed <lesed@DESKTOP-D9HIKHT>
2026-06-15 23:17:07 +0300

    Add project files.

---
+++ minesweeper/minesweeper.cpp
@@ -0,0 +1,12 @@
+﻿// minesweeper.cpp : Defines the entry point for the application.
+//
+
+#include "minesweeper.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
